ed69dca440b9e2bc9779317d09b5228d102cda40
mike <nzmichaelwu@hotmail.com>
2022-10-08 20:05:12 +1100

    first iteration of the web app is done, can do filters and display data accordingly now

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 import yaml
 import subprocess
+import datetime as dt
 from flask import Flask, jsonify
 from flask_cors import CORS
 from backend.database import get_hotcopper, get_marketindex
@@ -18,14 +19,6 @@ from helper_funcs.helper_funcs import util as hf
 
 cfg = hf.DotDict(yaml.safe_load(open('config.yml')))
 
-# pyfile = hf.fileloc(globals())
-# logger = hf.setup_logging(__name__, f'{cfg.out.LOGS}/{pyfile}.txt', add_ts=True); logger.l = lambda ls: logger.lbase(ls, globals())
-# logname = logger.handlers[1].baseFilename.split('/')[-1].split('.')[0]
-
-# gitstat = subprocess.run(['git', 'rev-parse', '--verify', 'HEAD'], capture_output=True)
-# git_commit_short_sha = gitstat.stdout[:7].decode('utf-8')
-# logger.l('git_commit_short_sha')
-# logger.info(f"Git Branch:\n{subprocess.check_output(['git', 'branch']).decode('utf8')}")
 
 # configuration
 DEBUG = True
@@ -51,6 +44,13 @@ df_all = df_market_idx.merge(df_hotcopper, how='left', on='ticker')
 
 df_table = df_all[['ticker', 'name', 'price', 'market_cap_mod', 'announcement', 'price_sensitive', 'date_time']] \
   .rename(columns={'market_cap_mod': 'market_cap', 'date_time': 'announcement_time'})
+
+# '''
+#   below block of code is to convert the time column to the right format with right date type
+# '''
+# df_table['announcement_time'] = pd.to_datetime(df_table['announcement_time'])
+# df_table['announcement_time'] = df_table['announcement_time'].dt.strftime('%d/%m/%Y %H:%M')
+
 df_table[['announcement', 'price_sensitive', 'announcement_time']] = df_table[['announcement', 'price_sensitive', 'announcement_time']].fillna(value='')
 
 df_table_dict = df_table.to_dict('records') # convert the pandas df into a list of dict
--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -12,15 +12,53 @@
             ></v-text-field>
           </v-row>
         </v-col>
+        <v-col offset-md="3" style="padding-top: 30px">
+          <v-row class="pa-6">
+            <label for="" style="padding-right: 10px">From:</label>
+            <input type="datetime-local" v-model="startDate" style="padding-right: 10px">
+            <label for="" style="padding-left: 20px; padding-right: 10px">To:</label>
+            <input type="datetime-local" v-model="endDate">
+          </v-row>
+        </v-col>
       </v-row>
     </v-container>
     <v-data-table
       :headers="headers"
-      :items="items"
+      :items="filteredAnnouncements"
       :search="search"
       :items-per-page="10"
       class="elevation-1"
-    ></v-data-table>
+    >
+      <template v-slot:[`header.price_sensitive`]="{ header }">
+        {{ header.text }}
+        <v-menu offset-y :close-on-content-click="false">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon small :color="priceSensitive ? 'primary' : ''">
+                mdi-filter
+              </v-icon>
+            </v-btn>
+          </template>
+          <div style="background-color: white; width: 280px">
+            <v-select
+              v-model="priceSensitive"
+              label="Select"
+              :items="['PRICE SENSITIVE', 'NOT PRICE SENSITIVE']"
+            ></v-select>
+            <v-btn
+              @click="priceSensitive = ''"
+              small
+              text
+              color="primary"
+              class="ml-2 mb-2"
+            >Clean</v-btn>
+          </div>
+        </v-menu>
+      </template>
+    </v-data-table>
+    <v-container>
+      Value: {{startDate}}
+    </v-container>
   </v-app>
 </template>
 
@@ -32,6 +70,9 @@ export default {
   data(){
     return {
       search: '',
+      priceSensitive: null,
+      startDate: null,
+      endDate: null,
       headers: [
         { text: "Ticker", value: 'ticker'},
         { text: "Name", value: 'name'},
@@ -41,27 +82,69 @@ export default {
         { text: "Price Sensitive", value: 'price_sensitive'},
         { text: "Announcement Time", value: 'announcement_time'},
       ],
-      items: []
+      announcements: []
     }
   },
   created(){
     axios
       .get('http://localhost:1234/')
       .then(response => (
-        this.items = response.data.items,
-        console.log(response.data.items)
+        this.full_data = response.data.items,
+        this.announcements = this.full_data
+        // console.log(response.data.items)
       ))
         // 
   },
-  // computed:{
-  //   filteredRows() {
-  //     return this.items.filter(item => {
-  //       const name = item.name.toLowerCase();
-  //       const searchTerm = this.filter.toLowerCase();
+  computed: {
+    filteredAnnouncements() {
+      
+      const conditions = [];
 
-  //       return name.includes(searchTerm)
-  //     });
-  //   }
-  // }
+      if (this.priceSensitive) {
+        conditions.push(this.filterPriceSensitive);
+      }
+
+      if (this.startDate || this.endDate) {
+        conditions.push(this.filterAnnouncementTime);
+      }
+
+      if (conditions.length > 0){
+        return this.announcements.filter((announcement) => {
+          return conditions.every((condition) => {
+            return condition(announcement)
+          })
+        })
+      }
+      console.log(this.announcements)
+      return this.announcements
+    }
+  },
+  methods: {
+    formatDate(date){
+      if (!date) return date
+      const date_mod = date.replace("T", " ")
+      return date_mod
+    },
+    filterAnnouncementTime(item) {
+      let startDate = this.formatDate(this.startDate);
+      let endDate = this.formatDate(this.endDate);
+      
+      const annnouncementDate = item.announcement_time
+      // console.log(annnouncementDate)
+      // console.log(startDate)
+      if (startDate && endDate) {
+        return annnouncementDate >= startDate && annnouncementDate <= endDate;
+      }
+      if (startDate && !endDate) {
+        return annnouncementDate >= startDate;
+      }
+      if (!startDate && endDate) {
+        return annnouncementDate <= endDate;
+      }
+    },
+    filterPriceSensitive(item) {
+      return item.price_sensitive == this.priceSensitive
+    }
+  }
 }
 </script>
