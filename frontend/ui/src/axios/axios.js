d723c18f6011808647cd09781c20678a5a7eb580
famagez <robert.t.clifford@gmail.com>
2023-01-28 15:56:28 +1100

    make nginx work (#7)

--- frontend/ui/src/axios/axios.js
+++ frontend/ui/src/axios/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
-const API_URL = process.env.API_URL || 'http://localhost/api';
+const API_URL = process.env.API_URL || 'http://localhost/';
 
 export default axios.create({
   baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.token}`,
+  },
 });
