f28a915a1786ea9061bafb5444fb315b0aadfb29
mike <nzmichaelwu@hotmail.com>
2023-01-31 19:48:16 +1100

    removed reference of API_URL

--- frontend/ui/src/axios/axios.js
+++ frontend/ui/src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.API_URL || 'http://localhost/';
+// const API_URL = process.env.API_URL || 'http://localhost/';
 
 export default axios.create({
   baseURL: '',
