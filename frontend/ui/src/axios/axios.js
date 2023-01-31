9c25add6d03f216f02710ba4f67b0cce09a467f6
Michael Wu <nzmichaelwu@hotmail.com>
2023-01-31 19:56:16 +1100

    merge changes from env_var branch to main (#9)
    * including the API_URL env var in docker compose
    
    * made baseURL in axios.js equals to empty str
    
    * removed reference of API_URL
    
    * tidied up docker-compose.yml

--- frontend/ui/src/axios/axios.js
+++ frontend/ui/src/axios/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const API_URL = process.env.API_URL || 'http://localhost/';
+// const API_URL = process.env.API_URL || 'http://localhost/';
 
 export default axios.create({
-  baseURL: API_URL,
+  baseURL: '',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.token}`,
