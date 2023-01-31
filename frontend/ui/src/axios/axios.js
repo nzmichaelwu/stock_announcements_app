875e33d59f171ec8b775512372f7a98e33e154bf
mike <nzmichaelwu@hotmail.com>
2023-01-31 19:38:52 +1100

    made baseURL in axios.js equals to empty str

--- frontend/ui/src/axios/axios.js
+++ frontend/ui/src/axios/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const API_URL = process.env.API_URL || 'http://localhost/';
 
 export default axios.create({
-  baseURL: API_URL,
+  baseURL: '',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.token}`,
